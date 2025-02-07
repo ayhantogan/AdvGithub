ef3712699202628b0540d1e2c0d5bd35d1ddfa9c
ayhantogan <ayhantogan@gmail.com>
2025-02-07 19:21:07 +0000

    d2

--- src/Book.java
+++ src/Book.java
@@ -3,5 +3,13 @@ public class Book {
     //degisiklik 2
     public String name;
 
+    //yeni bir field
+    public String author;
+
+    //calismaya devam.......
+    //......
+    //....... tamamlandi
+
+
 
 }
